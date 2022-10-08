d2bb81352b363731194918d0c0e76e903ca6cac4
SajidSarwar12 <sajidsrwr@gmail.com>
2022-10-08 12:40:13 +0200

    typist removed

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,6 +1,6 @@
 
-import React, {Component} from 'react';
-import Typist from 'react-typist';
+import React from 'react';
+//import Typist from 'react-typist';
 import './banner.css';
 
 
@@ -10,9 +10,11 @@ export default()=>{
         <div className="bannerBackground">
            <div className="bannerContent">
              
-             <Typist> <h1>Hi, I am <br/><span className="name">Muhammad Sajid Sarwar</span></h1>
+             {/* <Typist> */}
+                 <h1>Hi, I am <br/><span className="name">Muhammad Sajid Sarwar</span></h1>
               <h2>Front end Developer | Reactjs</h2>
-              </Typist>
+              
+              {/* </Typist> */}
            </div>
            <div></div>
         </div>
